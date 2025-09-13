c4f578662262d3c978ce4a3c1ea4cc61806de9bd
kelsonalmeida <kelson.victr@gmail.com>
2025-09-13 09:16:19 -0300

    att produtoserviceimpl

--- src/main/java/br/com/meusistema/api/service/ProdutoServiceImpl.java
+++ src/main/java/br/com/meusistema/api/service/ProdutoServiceImpl.java
@@ -2,18 +2,37 @@ package br.com.meusistema.api.service;
 
 import br.com.meusistema.api.dtos.ProdutoRequestDTO;
 import br.com.meusistema.api.dtos.ProdutoResponseDTO;
+import br.com.meusistema.api.mapper.ProdutoMapper;
+import br.com.meusistema.api.model.Fornecedor;
+import br.com.meusistema.api.model.Produto;
+import br.com.meusistema.api.repository.FornecedorRepository;
 import br.com.meusistema.api.repository.ProdutoRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 @Service
 @RequiredArgsConstructor
 public class ProdutoServiceImpl implements ProdutoService {
 
     private final ProdutoRepository produtoRepository;
+    private final FornecedorRepository fornecedorRepository;
+    private final ProdutoMapper produtoMapper;
 
     @Override
     public ProdutoResponseDTO criarProduto(ProdutoRequestDTO produtoRequestDTO) {
-        return null;
+       Produto produto = produtoMapper.toEntity(produtoRequestDTO);
+       produto.setFornecedor
+               (buscarFornecedor(produtoRequestDTO.fornecedorId()));
+       return produtoMapper.toDTO(produtoRepository.save(produto));
+    }
+
+    private Fornecedor buscarFornecedor(Long id) {
+        return fornecedorRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException
+                                (HttpStatus.NOT_FOUND,
+                                "Fornecedor não encontrado"));
     }
 }
